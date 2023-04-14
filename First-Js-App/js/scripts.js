9f266215d6e71e29978d47bed6c6ea33cbad87fa
aidanh911 <67652153+aidanh911@users.noreply.github.com>
2023-04-13 23:38:22 -0500

    Add files via upload

---
+++ First-Js-App/js/scripts.js
@@ -0,0 +1,19 @@
+// alert('Hello World');
+//
+// let favoriteFood = 'Yakitori';
+//
+// document.write(favoriteFood);
+
+
+let pokemonList = [
+    {name: 'Bulbasaur', type: 'Grass', height: '7'},
+    {name: 'Charmander', type: 'Fire', height: '6'},
+    {name: 'Caterpie', type: 'Bug', height: '3'}
+];
+
+for (let i= 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + pokemonList[i].height);
+};
+
+
+
